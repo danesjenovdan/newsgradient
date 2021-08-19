0de61fc375b2f7b38fe4f51cf7389b4f9ba5a225
LepkoQQ <lepko.san@gmail.com>
2021-08-19 13:54:57 +0200

    newsletter improvements and fixes, add static files

--- api/news/templatetags/newsletter.py
+++ api/news/templatetags/newsletter.py
@@ -8,3 +8,9 @@ def base_url(url):
         return url
     slice_end = url.find('/', url.find('://') + 3)
     return url[0:slice_end]
+
+@register.filter
+def favicon_url(url):
+    if not url or not isinstance(url, str):
+        return 'https://www.google.com/s2/favicons?sz=32&domain_url=https://does.not.exist.newsgradient.org'
+    return f'https://www.google.com/s2/favicons?sz=32&domain_url={base_url(url)}'
