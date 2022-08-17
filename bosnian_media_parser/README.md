dd6d2e99ecedb1e78d3d08171054313fcecb3231
patricijab <patricija.brecko@gmail.com>
2022-08-17 09:57:25 +0200

    add parent spider class

--- bosnian_media_parser/README.md
+++ bosnian_media_parser/README.md
@@ -6,7 +6,18 @@
 
 ## RUN SPIDER
 crawl to stdout
-```scrapy crawl n1info```
+```bash
+scrapy crawl n1info
+```
 
 crawl to file
-```scrapy crawl n1info -o some.json```
+```bash
+scrapy crawl n1info -o some.json
+```
+
+## CREATING NEW SPIDERS (OR DEBUGGING)
+Scrapy provides a useful command to try parsing options in the shell.
+```bash
+scrapy shell <url>
+```
+After running this command Scrapy parses the website at given url and returns its contents in the `response` object. You can then run `response.css("...").get()` or whichever commands you need.
---
+++ bosnian_media_parser/bosnian_media_parser/spiders/spider.py
@@ -0,0 +1,100 @@
+import scrapy
+from datetime import datetime
+import locale
+locale.setlocale(locale.LC_ALL, "bs_BA.UTF-8")
+
+class CustomSpider(scrapy.Spider):
+
+  # spider settings
+  name = 'scraper_name'
+  allowed_domains = ['example.com']
+  start_urls = ['https://example.com/']
+
+  # medium data settings
+  medium_id = 1
+
+  # HOMEPAGE
+  # a list of css classes we use to select which DOM elements to parse
+  allowed_home_page_div_classes = ['class1', 'class2']
+
+  # ARTICLE PAGE
+  # css selector used to find the DOM element containing article title
+  news_title_class = 'selector-example h1'
+  # css selector used to find the DOM elements containing article contents
+  news_content_class = 'selector-example .content'
+  # array of strings that mark DOM elements which we should skip while collecting content
+  ignore_starts_words = ['Izvor:', 'Podijeli:', 'iPhone/iPad']
+  # skip the content found after this string
+  skip_after = 'Program N1 televizije možete pratiti UŽIVO'
+  # css selector used to find the date
+  date_element = 'selector-example .post-time'
+
+
+  def parse(self, response):
+    """ Parses the medium home page and calls function parse_news on all found links to articles. """
+
+    # sparsaj vse linke iz specifičnih div-ov iz home page-a
+    for cls in self.allowed_home_page_div_classes:
+        home_links = response.css(f'div.{cls} a')
+        yield from response.follow_all(home_links, self.parse_news)
+
+
+  def parse_news(self, response):
+    """ Parses the article page and yields the data. """
+
+    # pridobi naslov in vsebino novice
+    title = response.css(f'{self.news_title_class}').get()
+    content_list = response.css(f'{self.news_content_class}').getall()
+
+    # prečišči besedilo
+    cleard_content = self.skip_items_and_merge(content_list)
+
+    # parsanje datuma
+    date_strings = response.css(f'{self.date_element}').getall()
+
+    formated_date = self.parse_date(date_strings)
+
+    yield {
+        'title': title,
+        'content': cleard_content.strip(),
+        'array': content_list,
+        'medium_id': self.medium_id,
+        'url': response.url,
+        'date': formated_date,
+    }
+
+
+  def parse_date(self, date_strings):
+    """
+    Parses string date format '%d. %b %Y %H:%M' to datetime.
+    """
+
+    date = ' '.join(date_strings).replace('>', '').strip()
+
+    # če je mesec avgust se zamenja ime meseca ker je v localah napačno ime meseca
+    date = date.replace('avg', 'aug')
+
+    # preskoči page če nima datuma
+    if not date:
+        return
+
+    formated_date = datetime.strptime(date, '%d. %b %Y %H:%M')
+    return formated_date
+
+
+  def skip_items_and_merge(self, texts):
+    """ 
+    Parse article contents and merge them into a single string. 
+    Skip those elements that start with any of ignore_starts_words strings.
+    Skip all content after skip_after string.
+    """
+    
+    for i, text in enumerate(texts):
+        for word in self.ignore_starts_words:
+            if text.strip().startswith(word):
+                texts[i] = ''
+
+    content = ' '.join(map(str.strip, texts))
+    if self.skip_after in content:
+        content = content[:content.index(self.skip_after)]
+    return content
