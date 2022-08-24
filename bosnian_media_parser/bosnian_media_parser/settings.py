a251045a2cfb92a54caa10f13ada57226999e51a
Tomaž kunst <cofek0@gmail.com>
2022-08-24 18:09:14 +0200

    added heaers

--- bosnian_media_parser/bosnian_media_parser/settings.py
+++ bosnian_media_parser/bosnian_media_parser/settings.py
@@ -86,3 +86,6 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+ROBOTSTXT_OBEY = False
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1'
