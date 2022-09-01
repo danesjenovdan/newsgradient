0a208cb2c0ae8310d8d64fb658cd467b5d7ce0ba
Tomaž kunst <cofek0@gmail.com>
2022-09-01 11:43:24 +0200

    fixed jabuka date

--- bosnian_media_parser/bosnian_media_parser/spiders/jabuka.py
+++ bosnian_media_parser/bosnian_media_parser/spiders/jabuka.py
@@ -23,15 +23,34 @@ class JabukaSpider(CustomSpider):
     news_content_class = 'div.entry-content>p ::text'
     ignore_starts_words = []
     skip_after = ''
-    date_element = 'div.date>span ::text'
+    date_element = 'div.date>span.updated::text'
 
     def parse_date(self, date_strings):
         """
         Parses string date format '%d. %b %Y %H:%M' to datetime.
         """
         # preskoči page če nima datuma
+
+        months = {
+            'siječanja': '1',
+            'veljača': '2',
+            'ojužaka': '3',
+            'travanja': '4',
+            'svibanja': '5',
+            'lipanja': '6',
+            'srpanja': '7',
+            'kolovoza': '8',
+            'rujna': '9',
+            'listopada': '10',
+            'studena': '11',
+            'prosinca': '12',
+        }
+
         if not date_strings:
             return
-        date = date_strings[0].strip()
-
-        return None
+        date_str = date_strings[0].strip().split(' ')
+        print(date_str)
+        date_str[1] = months[date_str[1]]
+        date = ' '.join(date_str)
+        formated_date = datetime.strptime(date, '%d. %m %Y.')
+        return formated_date
