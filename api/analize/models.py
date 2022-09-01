b0e3d24f3c94aa47231de14c88c3c3b8aa39fdc3
Tomaž kunst <cofek0@gmail.com>
2022-09-01 13:57:46 +0200

    fixed models for scores and add admin

--- api/analize/models.py
+++ api/analize/models.py
@@ -30,6 +30,7 @@ class Media(models.Model):
 
 class Actor(models.Model):
     name = models.TextField(verbose_name=_('Name'))
+    parser_names = models.TextField(verbose_name=_('Parser names divided with |'))
 
     def __str__(self):
         return self.name
@@ -43,6 +44,19 @@ class Member(Actor):
     pass
 
 
+class PartyNewsMention(models.Model):
+    party = models.ForeignKey('Party', on_delete=models.CASCADE, related_name='parties_through', verbose_name=_('Party'))
+    news = models.ForeignKey('News', on_delete=models.CASCADE, related_name='partynews_through', verbose_name=_('News'))
+    score = models.IntegerField(null=True, blank=True, verbose_name=_('Score'))
+
+
+class MembersNewsMention(models.Model):
+    member = models.ForeignKey('Member', on_delete=models.CASCADE, related_name='members_through', verbose_name=_('Member'))
+    news = models.ForeignKey('News', on_delete=models.CASCADE, related_name='membernews_through', verbose_name=_('News'))
+    score = models.IntegerField(null=True, blank=True, verbose_name=_('Score'))
+
+
+
 class News(models.Model):
     title = models.TextField(verbose_name=_('Title'))
     content = models.TextField(verbose_name=_('Content'))
@@ -52,8 +66,8 @@ class News(models.Model):
     published_at = models.DateTimeField(null=True, blank=True, verbose_name=_('Published at'))
     parsed_at = models.DateTimeField(null=True, blank=True, verbose_name=_('Parsed at'))
     last_parsed_at = models.DateTimeField(null=True, blank=True, verbose_name=_('Last parsed at'))
-    parties = models.ManyToManyField('Party', related_name='news', verbose_name=_('Parties'))
-    members = models.ManyToManyField('Member', related_name='news', verbose_name=_('Members'))
+    parties = models.ManyToManyField('Party', related_name='news', through='PartyNewsMention', verbose_name=_('Parties'))
+    members = models.ManyToManyField('Member', related_name='news', through='MembersNewsMention', verbose_name=_('Members'))
 
     def __str__(self):
         return self.title
