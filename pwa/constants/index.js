3673353500ffae633c52288d7a0e8cdd6ab8b99a
LepkoQQ <lepko.san@gmail.com>
2023-11-07 18:42:19 +0100

    replace bih map svg

--- pwa/constants/index.js
+++ pwa/constants/index.js
@@ -16,20 +16,9 @@ export const SLANTS = {
 }
 
 export const TERRITORIES = [
-  [{ name: 'FBiH', svgIndex: 0 }],
-  [
-    { name: 'HBŽ', svgIndex: 1 },
-    { name: 'SŽ', svgIndex: 2 },
-    { name: 'ŽZH', svgIndex: 3 },
-    { name: 'ZDŽ', svgIndex: 4 },
-    { name: 'USŽ', svgIndex: 5 },
-    { name: 'HNŽ', svgIndex: 6 },
-    { name: 'ŽSB', svgIndex: 7 },
-    { name: 'BPŽ', svgIndex: 8 },
-    { name: 'TŽ', svgIndex: 9 },
-  ],
-  [{ name: 'RS', svgIndex: 10 }],
-  [{ name: 'Brčko', svgIndex: 11 }],
+  [{ name: 'FBiH', svgIndex: 1 }],
+  [{ name: 'RS', svgIndex: 2 }],
+  // [{ name: 'Brčko', svgIndex: 3 }],
 ]
 
 export const PARTIES = ['DF', 'HNS', 'NiP', 'SDA', 'SK', 'SDP BiH', 'NS - BHI KF', 'SNSD', 'SDS - PDP - ZA PRAVDU I RED']
